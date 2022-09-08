e5c99cb46d316b35b077ea4510d3679017a4cbce
Rachiid007 <91373823+Rachiid007@users.noreply.github.com>
2022-09-08 22:08:12 +0000

    Deploying to gh-pages from @ Rachiid007/Portfolio@792d1af98dbd8f0dad7ae92c03c1f26996bfe3da 🚀

---
+++ _nuxt/_...slug_.a3b9855f.js
@@ -0,0 +1,1 @@
+import{o,a as t,b as n,e as a}from"./entry.65ad4103.js";const u={__name:"[...slug]",setup(_){return(c,s)=>{const e=a;return o(),t("div",null,[n(e,{name:"not-found"})])}}};export{u as default};
---
+++ _nuxt/index.b3ae0071.js
@@ -0,0 +1,1 @@
+import{o as t,a as o,b as n,e as a}from"./entry.65ad4103.js";const m={__name:"index",setup(_){return(c,s)=>{const e=a;return t(),o("div",null,[n(e,{name:"custom"})])}}};export{m as default};

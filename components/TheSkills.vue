14f9151db619fb55997b1f5b1541b42d72c6a247
Rachiid007 <abderrachid.bellaali@gmail.com>
2022-09-14 10:02:02 +0200

    ♻️ refactor(skills): use an array instead of a hardcode value

--- components/TheSkills.vue
+++ components/TheSkills.vue
@@ -3,57 +3,51 @@ const skillsPos = ref(null)
 
 const skillsContent = ref(null)
 resiveScrollspy(1, skillsContent, skillsPos)
+
+const mySkills = [
+  {
+    name: 'JavaScript',
+    level: '75%',
+  },
+  {
+    name: 'Python',
+    level: '70%',
+  },
+  {
+    name: 'Linux / Git',
+    level: '75%',
+  },
+  {
+    name: 'Docker',
+    level: '75%',
+  },
+  {
+    name: 'HTML / CSS',
+    level: '80%',
+  },
+  {
+    name: 'Flutter / Dart',
+    level: '50%',
+  },
+  {
+    name: 'SQL',
+    level: '70%',
+  }
+]
 </script>
 
 <template>
   <article id="skills" ref="skillsContent">
     <h2 class="title" ref="skillsPos" id="test-skills">{{ $t('skills.title') }}</h2>
     <div id="skills-content">
-      <div class="column left">
-        <div class="bars">
+      <div v-for="skill in mySkills" class="bars">
           <div class="info">
-            <span>Python</span>
-            <span>75%</span>
+            <span>{{ skill.name }}</span>
+            <span>{{ skill.level }}</span>
           </div>
-          <div class="line python"></div>
-        </div>
-        <div class="bars">
-          <div class="info">
-            <span>JavaScript</span>
-            <span>80%</span>
+          <div class="line">
+            <div class="progress" :style="{ width: skill.level }"></div>
           </div>
-          <div class="line js"></div>
-        </div>
-        <div class="bars">
-          <div class="info">
-            <span>Linux / Git</span>
-            <span>70%</span>
-          </div>
-          <div class="line linux-git"></div>
-        </div>
-      </div>
-      <div class="column right">
-        <div class="bars">
-          <div class="info">
-            <span>HTML / CSS</span>
-            <span>80%</span>
-          </div>
-          <div class="line html-css"></div>
-        </div>
-        <div class="bars">
-          <div class="info">
-            <span>Flutter / Dart</span>
-            <span>50%</span>
-          </div>
-          <div class="line flutter"></div>
-        </div>
-        <div class="bars">
-          <div class="info">
-            <span>SQL</span>
-            <span>75%</span>
-          </div>
-          <div class="line sql"></div>
-        </div>
       </div>
     </div>
   </article>
@@ -64,18 +58,12 @@ resiveScrollspy(1, skillsContent, skillsPos)
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  align-content: center;
-  align-items: center;
-  gap: 10px 20px; /* row-gap column gap */
-}
-
-#skills-content .column {
-  width: calc(50% - 20px);
-  min-width: 330px;
+  gap: 17px 20px; /* row-gap column gap */
 }
 
 #skills-content .bars {
-  margin-bottom: 15px;
+  min-width: 330px;
+  width: calc(50% - 20px);
 }
 
 #skills-content .info {
@@ -89,36 +77,12 @@ resiveScrollspy(1, skillsContent, skillsPos)
 }
 
 #skills-content .line {
-  height: 0.8em;
+  height: 15px;
   width: 100%;
   background: lightgrey;
-  position: relative;
 }
-#skills-content .line::before {
-  content: '';
-  position: absolute;
+#skills-content .progress {
   height: 100%;
-  left: 0;
-  top: 0;
   background: crimson;
-}
-#skills-content .python::before {
-  width: 75%;
-}
-#skills-content .js::before {
-  width: 80%;
-}
-#skills-content .linux-git::before {
-  width: 70%;
-}
-
-#skills-content .html-css::before {
-  width: 80%;
-}
-#skills-content .flutter::before {
-  width: 50%;
-}
-#skills-content .sql::before {
-  width: 75%;
 }
 </style>
